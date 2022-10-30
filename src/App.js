77c26104b993166badf0d7d14fc6b354915177aa
ThiagoMarques <thiagossmarques@gmail.com>
2022-10-30 17:19:57 -0300

    insere exemplo de passagem de dados com array

--- src/App.js
+++ src/App.js
@@ -1,19 +1,4 @@
 const App = () => {
-  const livros = [
-    { nome: 'A Game of Thrones', ano: 1996 },
-    { nome: 'A Clash of Kings', ano: 1998 },
-    { nome: 'A Storm of Swords', ano: 2000 },
-  ]
-  return (
-    <ul>
-      {livros
-        .filter(({ ano }) => ano >= 1998)
-        .map(({ nome, ano }) => (
-          <li key={nome}>
-            {nome}, {ano}
-          </li>
-        ))}
-    </ul>
-  )
+  return <div></div>
 }
 export default App
